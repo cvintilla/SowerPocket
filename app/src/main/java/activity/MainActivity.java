df4fae31e9f3ab15458a87af9c9c23e7732bffd3
cvintilla <c.vintilla@gmail.com>
2017-11-01 20:50:17 -0400

    1.1

--- app/src/main/java/activity/MainActivity.java
+++ app/src/main/java/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.christian.socketpowerandroid;
+package activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.christian.socketpowerandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
